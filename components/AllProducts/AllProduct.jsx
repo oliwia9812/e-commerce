d039af6aa8994a2822fe7b50b63ee30661fa58d8
Oliwia <Oliwia.9812@gmail.com>
2022-06-03 13:15:30 +0200

    added about us page

--- components/AllProducts/AllProduct.jsx
+++ components/AllProducts/AllProduct.jsx
@@ -1,14 +1,35 @@
 import styles from './AllProducts.module.scss';
 import { urlFor } from '../../lib/client';
 import { IoCartOutline } from 'react-icons/io5';
+import { useState } from 'react';
 
 const AllProducts = ({ products }) => {
 
-    console.log(products);
+    const [ showPopover, setShowPopover ] = useState(false);
+
+    const handleOpen = () => {
+        setShowPopover(prevState => !prevState);
+    }
 
     return (
+    
        <section className={styles.allProducts}>
         <div className={styles.container}>
+            <button onClick={handleOpen} className={styles.sortButton}> Sort by</button>
+           {showPopover &&  <div className={styles.popover}>
+                <div className={styles.popover__content}>
+                    <ul className={styles.popover__messageWrapper}>
+                        <li className={styles.popover__message}>best selling</li>
+                        <li className={styles.popover__message}>alphabetically, a-z</li>
+                        <li className={styles.popover__message}>alphabetically, z-a</li>
+                        <li className={styles.popover__message}>price, low to high</li>
+                        <li className={styles.popover__message}>price, high to low </li>
+                        <li className={styles.popover__message}>date, old to new</li>
+                        <li className={styles.popover__message}>date, new to old</li>
+                    </ul>
+                </div>
+            </div>
+            }
             {products.length >= 1 && products.map(product => (
                  <div className={styles.product}>
                      <div className={styles.product__imageContainer}> 
--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,38 @@
 import React from 'react';
+import Headline from '../components/Headline/Headline';
+import headlineImage from '../assets/headline_about.jpg';
+import styles from './about.module.scss';
+import founderImage from '../assets/about_founder.jpg';
+import historyImage from '../assets/about_history.jpg';
 
 const About = () => {
     return (
-        <h1>ABOUT</h1>
+        <>
+            <Headline src={headlineImage.src} title='About us' /> 
+            <div className={styles.quote}>
+                <h2>Time is what we want most, but what we use worst. ~ William Penn</h2>
+            </div>
+            <section className={styles.about}>
+                <div className={styles.about__flex}>
+                    <div className={styles.about__imageWrapper}>
+                        <img className={styles.about__image} src={historyImage.src} />
+                    </div>
+                    <div className={styles.about__contentWrapper}>
+                        <h2 className={styles.about__title}>Our history</h2>
+                        <p className={styles.about__content}>One day when shopping for a new planner I thought to myself – why not design something of my own? Something that would combine all of the productivity techniques I love, while also have a touch of pleasant aesthetic to it. And that’s how the journey had begun! I love helping women become a better version of themselves daily. Welcome! I’m so glad you’ve found us.  </p>
+                    </div>
+                </div>
+                <div className={styles.about__flex}>
+                    <div className={styles.about__imageWrapper}>
+                        <img className={styles.about__image} src={founderImage.src} />
+                    </div>
+                    <div className={styles.about__contentWrapper}>
+                        <h2 className={styles.about__title}>Our founder</h2>
+                        <p className={styles.about__content}>I’ve always been a planning junkie, but this period of my life was an especially obsessive journey. I spent a year on reading self-help books on productivity, time management, goal-setting tactics, financial planning, habit formation, decluttering, you name it… It helped me feel like my life was a bit more organized and put together. Little did I know that I was on my way to finding my purpose.  </p>
+                    </div>    
+                </div>
+            </section>
+        </>
     )
 }
 
