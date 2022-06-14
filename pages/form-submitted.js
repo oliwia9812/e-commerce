d8d546f4f0a3d864c07e10a46d28ac5cfe99b020
Oliwia <Oliwia.9812@gmail.com>
2022-06-14 13:41:13 +0200

    added form-submitted page

--- pages/form-submitted.js
+++ pages/form-submitted.js
@@ -1,8 +1,17 @@
 import styles from './styles/form-submitted.module.scss';
+import image from '../assets/form-submitted.jpg';
 
 const FormSubmitted = () => {
     return (
-        <h1>It's submitted page</h1>
+        <section className={styles.formSubmitted}>
+            <div className={styles.wrapper}>
+                <h1 className={styles.h1}>Thank you</h1>
+                <div className={styles.imageContainer}>
+                    <img className={styles.image} src={image.src} />
+                </div>
+                <p className={styles.info}>Your form has been sent successfully! </p>
+            </div>
+        </section>
     )
 }
 
